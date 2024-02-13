fb00228c7f888108227a1414a17c30aef1f29dfb
Alex <zalexkrylovz@gmail.com>
2024-02-13 22:38:48 +0300

    Update Create_instance_1.tf

--- terraform/Create_instance_1.tf
+++ terraform/Create_instance_1.tf
@@ -1,14 +1,25 @@
 provider "google" {
   credentials = file(var.credentials_file)
-  project     = "tidal-mason-406418"
-  region      = "us-central1"
-  zone        = "us-central1-b"
+  project = "tidal-mason-406418"
+  region  = "us-central1"
+  zone    = "us-central1-b"
+}
+variable "gce_ssh_user" {
+  type    = string
+  default = "default"
 }
 
+variable "public_ssh_key" {
+  type    = string
+  default = ""
+}
 resource "google_compute_instance" "default" {
   provider     = google
   name         = "default"
   machine_type = "e2-medium"
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${var.public_ssh_key}"
+  }
   network_interface {
     access_config {
       network_tier = "PREMIUM"
@@ -21,13 +32,13 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-10"
-      size  = 30
+      size = 30 
     }
+    
   }
-  metadata_startup_script = file("./create_soft.sh")
+  metadata_startup_script = "${file("./create_soft.sh")}"
 }
-
+  
 output "instance_ip" {
   value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-}
-
+} 
