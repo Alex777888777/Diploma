5b4ea0c2fe3a97a5efcf0fbb6c67d055d7246e53
Alex <zalexkrylovz@gmail.com>
2024-02-13 11:23:45 +0300

    Update create_soft.sh

--- terraform/create_soft.sh
+++ terraform/create_soft.sh
@@ -19,4 +19,4 @@ sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-echo "${privateKey}" >> authorized_keys
+echo "${privateKey}" >> /home/zfreyjaz/.ssh/authorized_keys
