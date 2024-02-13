b4b57db5314db4d5f043e764555e4c942f02aea8
Alex <zalexkrylovz@gmail.com>
2024-02-13 12:31:49 +0300

    Update Create_instance_1.tf

--- terraform/Create_instance_1.tf
+++ terraform/Create_instance_1.tf
@@ -1,9 +1,10 @@
 provider "google" {
   credentials = file(var.credentials_file)
-  project = "tidal-mason-406418"
-  region  = "us-central1"
-  zone    = "us-central1-b"
+  project     = "tidal-mason-406418"
+  region      = "us-central1"
+  zone        = "us-central1-b"
 }
+
 resource "google_compute_instance" "default" {
   provider     = google
   name         = "default"
@@ -20,12 +21,13 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-10"
-      size = 30 
+      size  = 30
     }
   }
-  metadata_startup_script = "${file("./create_soft.sh")}"
+  metadata_startup_script = file("./create_soft.sh")
 }
+
 output "instance_ip" {
-  value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
 
