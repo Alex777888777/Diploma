f77fecb6165ff03bf7f1db1247ef05952a01e17c
Alex <zalexkrylovz@gmail.com>
2024-02-14 00:47:05 +0300

    Update Create_instance_1.tf

--- terraform/Create_instance_1.tf
+++ terraform/Create_instance_1.tf
@@ -11,7 +11,7 @@ variable "gce_ssh_user" {
 
 variable "public_ssh_key" {
   type    = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDM4VhUWDiTDGm+3qOBBf9WSP+Bw+Nej7+XbeUYAWmc7hRgMo6QFYO+k4yGpzTKm4HVAjyyXlbU+B5/u+bcD0Iuu71k5duaV1aeRBbW3kYYvfC5YtY5558/DYKxStvGn0MTNWYbohACGIQPF9VYGBaJyBlts6fgYQyYxhBpEP2U5Ah3OgEeMsxXO+Rau1oV4YlHpuVhL25V2uh/HOzHZCWy/Yo7PhCVqvPlT0ut2Tzoh/fCASDGYRLrqtUPBH29Sjd95DISE5AdDjJiXeHVr6ERXr1zdyJI7JF7PilyjLcTBy2vKTxl3Yc6XKNEhyW8HE7/H4irIbQhFC98/amXAIXswYr1TwE+UNpStBrwFIS1VolsxKVrSXXsMKj+mNrvhnudV6AMiDORO6TnU/8zMBq7A4r+PO4NcvhtUsEqCBz6QPhYEyVaIdT34v5SLjLPcDahSKPa8PHtR+pWk0j1URi3hTfo7OJEv/1lqhIPx5rnb9WHIPN4LGX2rbCoKIY9S0M= zfreyjaz@instance-11.us-central1-b.c.tidal-mason-406418.internal"
+  default = ""
 }
 resource "google_compute_instance" "default" {
   provider     = google
