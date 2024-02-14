41c250dd0b35ecaa09d77bf23dfe249c4ce56271
Alex <zalexkrylovz@gmail.com>
2024-02-14 18:46:24 +0300

    Update create_soft.sh

--- terraform/create_soft.sh
+++ terraform/create_soft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sudo apt-get update && sudo apt-get upgrade -y
 sudo apt -y install git
-sudo apt -y install default-jdk
+#sudo apt -y install default-jdk
 sudo apt-get update
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
