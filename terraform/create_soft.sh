1c12f8d13df598c3a4e4f302417262fd605f530b
Alex <zalexkrylovz@gmail.com>
2024-02-14 16:31:27 +0300

    Update create_soft.sh

--- terraform/create_soft.sh
+++ terraform/create_soft.sh
@@ -20,4 +20,4 @@ sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-pwd && ls
+git clone 
